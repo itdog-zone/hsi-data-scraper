d3c2b91d2b5e18b5cbd5dba1118d3311498d2bed
sdpaydev@gmail.com <sdpaydev@gmail.com>
2025-06-24 18:07:11 +0800

    Add holiday check and HkGov module for holiday data retrieval

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import Args from './lib/Args.js';
 import Hsi from './lib/Hsi.js';
 import GitHub from './lib/github.js';
 import Utils from './lib/Utils.js';
+import HkGov from './lib/HkGov.js';
 
 
 (
@@ -23,7 +24,19 @@ import Utils from './lib/Utils.js';
         }
         console.log(`curr Date: ${date.toFormat('yyyy-MM-dd')}`);
 
+        // get holidays
+        const holidays = await HkGov.getHolidayList();
+        if (holidays.find(x => x.date === date.toFormat('yyyyMMdd')) !== undefined) {
+            console.log(`skip on holiday: ${date.toFormat('yyyy-MM-dd')}`)
+            process.exit(0)
+        }
+        // exit if sat and sun
+        if (date.weekday === 6 || date.weekday === 7) {
+            console.log(`skip on sat and sun: ${date.toFormat('yyyy-MM-dd')} (${date.toFormat('ccc')})`)
+            process.exit(0)
+        }
 
+        // Loop for 3 main index
         const idxList = ['hsi', 'hscei', 'hstech']
         const github = new GitHub({ owner: process.env.GITHUB_OWNER, repo: process.env.GITHUB_REPO, token: process.env.GITHUB_TOKEN });
         for (const _idx of idxList) {
@@ -41,7 +54,7 @@ import Utils from './lib/Utils.js';
 
             if (fs.existsSync(pdfToProcess)) {
 
-                const indexJsonData = await Hsi.getDataFromPdf({filePath: pdfToProcess, index: _idx, date: date.toFormat('yyyyMMdd')})
+                const indexJsonData = await Hsi.getDataFromPdf({ filePath: pdfToProcess, index: _idx, date: date.toFormat('yyyyMMdd') })
                 fs.writeFileSync(jsonToProcess, JSON.stringify(indexJsonData, null, 2));
 
                 console.log(`process to upload file: ${pdfToProcess}`)
@@ -49,12 +62,12 @@ import Utils from './lib/Utils.js';
                 await Utils.delay(1000);
                 await github.uploadConstituentsPdf({ path: `hkex/constituents/json/${filename}.json`, filePath: jsonToProcess });
                 await Utils.delay(1000);
-                if(Args.getValue('keep') !== 'true'){
+                if (Args.getValue('keep') !== 'true') {
                     fs.rmSync(pdfToProcess);
                     fs.rmSync(jsonToProcess);
                 }
             }
         }
-
+        process.exit(0)
     }
 )();
