8b2f73acea943e5a63e2d7e767dd696e27a61bc7
sdpaydev@gmail.com <sdpaydev@gmail.com>
2025-06-25 19:18:53 +0800

    checking of version running

--- index.js
+++ index.js
@@ -10,10 +10,14 @@ import HkGov from './lib/HkGov.js';
 
 (
     async () => {
+        console.log('Running with Node.js version:', process.version);
+        
         // setup date for processing
-        let date = DateTime.now().startOf('day');
+        // data generate on next day 0100 -> scrape the previous day data
+        let date = DateTime.now().plus({ days: -1 }).startOf('day');
         console.log(`init Date: ${date.toFormat('yyyy-MM-dd')}`);
 
+        // args input, override the default date
         const dateInput = Args.getValue('date');
         if (dateInput) {
             const inputDate = DateTime.fromISO(dateInput);
@@ -23,24 +27,50 @@ import HkGov from './lib/HkGov.js';
         }
         console.log(`process Date: ${date.toFormat('yyyy-MM-dd')} (${date.toFormat('ccc')})`);
 
+        const dateList = []
+
         // get holidays
         const holidays = await HkGov.getHolidayList();
         if (holidays.find(x => x.date === date.toFormat('yyyyMMdd')) !== undefined) {
             const msg = `skip on holiday: ${date.toFormat('yyyy-MM-dd')}`
             console.log(msg)
             await Utils.sendMessage({ msg: msg });
-            process.exit(0)
         }
-        // exit if sat and sun
-        if (date.weekday === 6 || date.weekday === 7) {
+        else if (date.weekday === 6 || date.weekday === 7) {
             const msg = `skip on sat and sun: ${date.toFormat('yyyy-MM-dd')} (${date.toFormat('ccc')})`
+            console.log(msg)
+            await Utils.sendMessage({ msg: msg });
+        } else {
+            dateList.push(date)
+        }
+
+        if (dateList.length === 0) {
+            const msg = `No data process`
             console.log(msg)
             await Utils.sendMessage({ msg: msg });
             process.exit(0)
         }
 
-        // Loop for 3 main index
+
+
+        // create file list for processing
+        const fileList = []
         const idxList = ['hsi', 'hscei', 'hstech']
+        for (const dd of dateList) {
+            for (const _idx of idxList) {
+                fileList.push({
+                    source: 'hsi', fromFile: `https://www.hsi.com.hk/static/uploads/contents/en/indexes/report/${_idx}/constituents_${dd.toFormat('dLLLyy')}.pdf`,
+                    downloadFile: `./download/${dd.toFormat('yyyyMMdd')}_${_idx}.pdf`,
+                    jsonFile: `./download/${dd.toFormat('yyyyMMdd')}_${_idx}.json`
+                })
+            }
+        }
+
+        console.table(dateList)
+        console.table(fileList)
+        return;
+
+        // Loop for 3 main index
         const github = new GitHub({ owner: process.env.GITHUB_OWNER, repo: process.env.GITHUB_REPO, token: process.env.GITHUB_TOKEN });
         for (const _idx of idxList) {
             const filename = `${date.toFormat('yyyyMMdd')}_${_idx}`
